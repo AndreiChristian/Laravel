fde9b6c3cbede72538362566c1a034d371990152
Andrei Christian <andreichristiannetoiu@gmail.com>
2023-08-27 20:06:41 +0300

    innitial commit

--- book_store/routes/web.php
+++ book_store/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view("welcome");
+});
+
+Route::get("/hello", function () {
+    return "hello world";
 });
